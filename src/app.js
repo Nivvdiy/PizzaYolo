e43b6cd87782294c7dbe8f0973b7535de26fd0fb
Jeffrey Porcheron <Nivvdiy@gmail.com>
2017-01-14 21:07:24 +0100

    TP PizzaYolo
    - Ajout d'un systeme de scoreboard
    - Enregistrement des scores
    - Ajout d'un systeme des meilleurs score et de rang
    - Modification de la mise en page

--- src/app.js
+++ src/app.js
@@ -6,29 +6,69 @@ const pizzeriaService = new PizzeriaService(recipesService);
 let scoreBoard;
 
 // liste des recettes
-recipesService.getRecipesNames()
+recipesService.getRecipes()
 .then(recipes => {
     
     $('#recipes')
-        .html(recipes.map(recipe => `
-        <li data-recipe="${ recipe }">
-            ${ recipe.toUpperCase() }
-        </li>
+        .html(recipes.sort((recipe1, recipe2) => {
+            return recipe1.name < recipe2.name ? -1:1;
+        }).map(recipe => `
+        <div class="btn-group col-lg-12 dropup">
+            <button type="button" class="btn btn-block btn-sm btn-warning dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">${ recipe.name.toUpperCase() }
+                <span class="caret"></span>
+                <span class="sr-only">Toggle Dropdown</span>
+            </button>
+            <ul class="dropdown-menu">`+
+                recipe.toppings.map(topping => `
+                    <li>- ${topping}</li>
+                `).join('')
+            +`</ul>
+        </div>
         `).join(''));
-
-    $('#recipes li').on('click', function () {
-        send($(this).data('recipe'));
-        //console.log($(this).data('recipe'));
-    });
 
 });
 
+pizzeriaService.getTopList()
+.then(users => {
+    console.log(users);
+    while(users.length<5){
+        users.push({"name":"", "rank":"Non classé", "score":NaN});
+    }
+    $('#toplist')
+        .html($('#toplist').html()
+        +users.filter((user, index) => {
+            return index < 5;
+        }).map(user => `
+        <tr>
+            <td>${user.name}</td>
+            <td>${user.score}</td>
+            <td>${user.rank}</td>
+        </tr>
+        `));
+});
+
+pizzeriaService.getUserTopList("Nivvdiy")
+.then(user => {
+    $('#userRank').html('Rank: '+user.rank);
+    $('#username').html(user.name);
+    $('#userToplist')
+        .html($('#userToplist').html()
+        +user.scores.filter((score, index) => {
+            return index < 5;
+        }).map(score => `
+        <tr>
+            <td colspan="3">${score}</td>
+        </tr>
+        `).join(''));
+});
 
 recipesService.getToppings()
 .then(toppings => {
     $('#toppings')
-        .html(toppings.map(topping => `
-        <button data-topping="${ topping }" class="btn">${ topping }</button>
+        .html(toppings.sort((topping1, topping2) => {
+            return topping1 < topping2 ? -1:1;
+        }).map(topping => `
+        <button data-topping="${ topping }" class="btn btn-info btn-sm col-md-3">${ topping }</button>
         `));
     $('#toppings button').click(function () {
        addTopping($(this).data('topping'));
@@ -40,10 +80,9 @@ let currentPizza = {};
 $('#startbtn').click(function () {
     scoreBoard=0;
     $('#scoreBoard').html(scoreBoard);
-    pizzeriaService.start(5000);
+    pizzeriaService.start(3000, clearCurrentToppings);
     $(this).hide();
-    $('#score').show();
-    $('#pool').show();
+    $('#sendbtn').show();
     currentPizza = {
         toppings: []
     };
@@ -56,6 +95,15 @@ $('#sendbtn').click(function () {
         }
     }
 });
+
+function clearCurrentToppings(reset=false){
+        currentPizza.toppings = [];
+        $('#scoreBoard').html(scoreBoard);
+        $('#currentTopping').html(currentPizza.toppings.join('<br>'));
+        if(reset){
+            currentPizza = {};
+        }
+}
 
 function addTopping(topping) {
     if(currentPizza.hasOwnProperty('toppings')){
@@ -70,14 +118,12 @@ function addTopping(topping) {
 
 function send(){
     recipesService.getPizzaRecipeName(currentPizza).then(recipeName =>{
-        console.log(recipeName);
         if(pizzeriaService.sendPizza(recipeName)){
             scoreBoard +=5;
         } else {
             scoreBoard -=2;
         }
-        currentPizza.toppings = [];
         $('#scoreBoard').html(scoreBoard);
-        $('#currentTopping').html(currentPizza.toppings.join('<br>'));
+        clearCurrentToppings();
     })
 }
--- src/services/pizzeria.service.js
+++ src/services/pizzeria.service.js
@@ -5,15 +5,81 @@ export class PizzeriaService {
         this.recipesService = recipesService;
     }
 
-    start (time) {
+    getUsers(){
+        if (this.users) return Promise.resolve(this.users);
+
+        return fetch('http://localhost:3000/users')
+        .then(response => response.json())
+        .then(users => this.users = users);
+    }
+
+    getTopList(){
+        /*
+        from user => {id:id, name:name, scores:[scoresList]}
+        to user => {rank:rank, name:name, score:bestScore}
+        */
+        return this.getUsers().then(usersList => {
+            return usersList.map(user => {
+                let userF = user;
+                while(userF.scores.length<5){
+                    userF.scores.push("Aucun score");
+                }
+                userF.score = user.scores.sort((score1,score2) => {
+                    if(score1<score2){
+                        return 1;
+                    } else {
+                        return -1;
+                    }
+                })[0];
+                return userF;
+            }).sort((user1,user2) => {
+                if(user1.score>user2.score){
+                    return -1;
+                } else {
+                    return 1;
+                }
+            }).map((user, index) => {
+                user.rank = index+1;
+                return user;
+            });
+        });
+    }
+
+    getUserTopList(username){
+        if(!username){
+            this.user = {"rank":"Non classé", "name":"Guest", "scores":["Aucun score","Aucun score","Aucun score","Aucun score","Aucun score"]};
+            return Promise.resolve(this.user);
+        }
+        this.user = {"rank":"Non classé", "name":username, "scores":["Aucun score","Aucun score","Aucun score","Aucun score","Aucun score"]};
+        return this.getTopList().then((users) => {
+            return users.filter(user => {
+                return user.name.toLowerCase()===username.toLowerCase();
+            }).reduce((acc, user) => {
+                if(user){
+                    this.user = user;
+                } else {
+                    this.user = {"rank":"Non classé", "name":username, "scores":["Aucun score","Aucun score","Aucun score","Aucun score","Aucun score"]};
+                }
+                return this.user;
+            },{"rank":"Non classé", "name":username, "scores":["Aucun score","Aucun score","Aucun score","Aucun score","Aucun score"]});
+        });
+    }
+
+    start (time, clearCurrentToppings) {
+        this.pooldiv = $('#pool .pizzas');
+        this.pooldiv.html("");
         // every time seconds add a new recipe name to the pool
         this.recipesService.getRecipesNames()
         .then(recipesNames => {
             this.intervalId = setInterval(() => {
-                this.pooldiv = $('#pool .pizzas');
                 if (this.pool.length >= 10) {
                     this.pooldiv.html('GAME OVER');
+                    $('#startbtn').show();
+                    $('#sendbtn').hide();
+                    clearCurrentToppings(true);
+                    this.pool = [];
                     clearInterval(this.intervalId);
+                    this.updateUserDB();
                 } else {
                 const index = Math.floor(Math.random() * recipesNames.length);
                 const recipeName = recipesNames[index];
@@ -23,6 +89,40 @@ export class PizzeriaService {
             }
             }, time);
         })
+    }
+
+    updateUserDB(){
+        if(this.user.name!=="Guest"){
+            let user = {};
+            let method;
+            let uri = "http://localhost:3000/users/";
+            if(this.user.id){
+                user.id = this.user.id;
+                uri += this.user.id;
+                method = "PUT";
+            } else {
+                method = "POST";
+            }
+            user.name = this.user.name;
+            user.scores = this.user.scores.filter(score =>{
+                return score!=="Aucun score";
+            });
+            user.scores.push(parseInt($('#scoreBoard').html()));
+            $.ajax({
+                type : method,
+                url : uri,
+                //contentType: 'application/json; charset=UTF-8',
+                dataType: 'json',
+                data: JSON.stringify(user),
+                contentType: 'application/json',
+                success : function(result) {
+                    window.location.href = "./";
+                },
+                failure : function(result) {
+                    console.log("Erreur " + result);
+                }
+            });
+        }
     }
 
     // { id: 1, toppings: ['', ''] }
